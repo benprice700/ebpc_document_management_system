daaacc9c3102e8be458762d2fa3c9665c3d96471
yve <631555801@qq.com>
2022-10-06 15:09:44 +1300

    with database

--- app.js
+++ app.js
@@ -18,27 +18,60 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 //uploading package
 var formidable = require('formidable');
+//sqlite
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(path.join(__dirname, '/db/ebpc_document_management_system.db3'))
 
 //to store the users
-const userData = []
+//const userData = []
 // get user by username
-const findByUsername = username => userData.find(user => user.username ===username)
+//const findByUsername = username => userData.find(user => user.username ===username)
+// app.get('/users', (req, res) =>{
+//     db.all("select * from 'user'", [], (err, rows) => {
+//         if (err == null) {
+//             res.send(rows)
+//         } else {
+//             res.send(err)
+//         }
+//     })
+// })
+
+const queryUser = (username) => {
+    return new Promise((resolve, reject) => {
+        db.all('SELECT * FROM user WHERE name = ?', [username], function (err, rows) {
+            if (err) {
+                reject(err);
+            } else {
+                const userData = rows[0];
+                resolve({
+                    ...userData,
+                    username: userData.name
+                });
+            }
+
+        })
+    })
+}
 
 //checking the username and password
 passport.use(new LocalStrategy({}, async (username, password, done) => {
-    const user = findByUsername(username)
+    const user = await queryUser(username);
+    // const user = findByUsername(username)
     if(!user) {
+        //console.log('User not found')
         return done(null, false, {message: 'User not found'})
     } else if (!(await bcrypt.compare(password, user.password))) {
+        //console.log('Password is incorrect')
         return done(null, false, {message: 'Password is incorrect'})
     }
-    return done(null, user)
+    return done(null, user);
 }))
 passport.serializeUser((user, done) => {
     done(null, user.username)
 })
-passport.deserializeUser((username, done) => {
-    return done(null, findByUsername(username))
+passport.deserializeUser(async (username, done) => {
+    const user = await queryUser(username);
+    return done(null, user)
 })
 
 //Test commit and push
@@ -110,13 +143,28 @@ app.get('/reg', (req, res) => {
 //setting user info
 app.post('/reg', async (req, res) => {
     let user = req.body
-    userData.push({
-        username: user.username,
-        password: await bcrypt.hash(user.password, 10),
-        level:0,
-        isDeleted:0
+    let username = user.username
+    let password = await bcrypt.hash(user.password, 10)
+    db.all('SELECT name FROM user WHERE name = ?', [username], function (err, rows) {
+        if (rows.length != 0) {
+            //console.log('User is already exist')
+            return res.render('register', {messages: "User is already exist"})
+        } else if(user.password.length < 6 || user.password.length > 16) {
+            //console.log('Password length should between 6 and 16')
+            return res.render('register', {messages: "Password length should between 6 and 16"})
+        } else if (!user.password.match('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[\\S]{6,16}')) {
+            //console.log('Password should include different case letters and numbers')
+            return res.render('register', {messages: "Password should include different case letters and numbers  "})
+        } else{
+            db.run('INSERT INTO user(name, level, password) VALUES(?, ?, ?)', [username, 0, password], function (err) {
+                if (err) {
+                    return console.log('insert data error: ', err.message)
+                } else {
+                    res.redirect('/login')
+                }
+            })
+        }
     })
-    res.redirect('/login')
 })
 //login function
 app.post('/login', passport.authenticate('local', {
